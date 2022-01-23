a03160b0bb200e792de1d7ef589b325dd248b8c0
Juan Melo <juanda2884@gmail.com>
2022-01-22 20:55:19 -0500

    Crud casi listo

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,7 +1,9 @@
 export class Cliente {
   id: number = 0;
+  tipoid: string = '';
   nombre: string = '';
   apellido: string = '';
   createAt: string = '';
   email: string = '';
+  estado: string = '';
 }
